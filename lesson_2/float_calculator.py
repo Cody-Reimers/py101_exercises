875154fb34fb861a544e12c4c2aaecf98ebe5f35
Cody Reimers <ceriousackount@gmail.com>
2024-03-09 23:41:34 +0000

    Created new calculator, which is able to handle floating-point numbers and can also accept multiple consecutive operations before executing.

---
+++ lesson_2/float_calculator.py
@@ -0,0 +1,199 @@
+def valid_float(number_str):
+    try:
+        float(number_str)
+    except ValueError:
+        return False
+
+    return True
+
+def proper_grouping(particle_type):
+    if not types:
+        if particle_type == "number":
+            return True
+
+    last = types[-1]
+
+    match particle_type:
+        case "operator":
+            if last == "operator":
+                return False
+        case "number":
+            if last in ("integer", "float"):
+                return False
+
+    return True
+
+def calculate():
+    evaluation = equation[:]
+
+    if len(evaluation) == 1:
+        return simple_eval(types[0])
+    if types[-1] == "operator":
+        return simple_eval("operator")
+
+    operations_remaining = types.count("operator")
+
+    while operations_remaining:
+        if "**" in evaluation:
+            index = find_first(evaluation, ["**"])
+            mutate_lists(evaluation, index, "**")
+            operations_remaining -= 1
+            continue
+
+        if ("*" in evaluation) or ("/" in evaluation) or ("//" in evaluation):
+            index = find_first(evaluation, ["*", "/", "//"])
+            if evaluation[index] == "*":
+                mutate_lists(evaluation, index, "*")
+            elif evaluation[index] == "/":
+                mutate_lists(evaluation, index, "/")
+            elif evaluation[index] == "//":
+                mutate_lists(evaluation, index, "//")
+            operations_remaining -= 1
+            continue
+
+        if ("+" in evaluation) or ("-" in evaluation):
+            index = find_first(evaluation, ["+", "-"])
+            if evaluation[index] == "+":
+                mutate_lists(evaluation, index, "+")
+            elif evaluation[index] == "-":
+                mutate_lists(evaluation, index, "-")
+            operations_remaining -= 1
+            continue
+
+    return evaluation[0]
+
+def simple_eval(identity):
+    match identity:
+        case "integer":
+            return int(equation[0])
+        case "float":
+            return float(equation[0])
+        case _:
+            return "ERROR/INVALID"
+
+def find_first(evaluation, targets):
+    for element in evaluation[1::2]:
+        for target in targets:
+            if element == target:
+                return evaluation.index(target)
+
+    return None
+
+def mutate_lists(evaluation, i, op):
+    num1 = get_num(evaluation[i - 1], types[i - 1])
+    num2 = get_num(evaluation[i + 1], types[i + 1])
+
+    match op:
+        case "**":
+            evaluation[i - 1] = num1 ** num2
+        case "*":
+            evaluation[i - 1] = num1 * num2
+        case "/":
+            evaluation[i - 1] = num1 / num2
+        case "//":
+            evaluation[i - 1] = num1 // num2
+        case "+":
+            evaluation[i - 1] = num1 + num2
+        case "-":
+            evaluation[i - 1] = num1 - num2
+
+    if isinstance(evaluation[i - 1], int):
+        types[i - 1] = "integer"
+    elif isinstance(evaluation[i - 1], float):
+        types[i - 1] = "float"
+
+    del evaluation[i:i + 2]
+    del types[i:i + 2]
+
+def get_num(num_str, data_type):
+    match data_type:
+        case "integer":
+            return int(num_str)
+        case "float":
+            return float(num_str)
+
+print("""
+    This program can handle integer numbers and decimal numbers.
+    Other than numbers, this program supports the following symbols:
+    "+" (addition), "-" (subtraction), "*" (multiplication),
+    "/" (decimal divison), "//" (integer divison), and "**" (exponents).
+    
+    Each time the calculator asks you for input, you must:
+    enter only a number; enter a single valid operation symbol;
+    or input "delete", "quit", or "complete".
+    
+    Enter "delete" to remove the last number or operation symbol;
+    enter "quit" to let the calculator know you are done;
+    enter "complete" to run a calculation.
+    
+    The calculator will print what the calculation currently
+    looks like as you go, to improve interactivity.
+"""
+)
+
+VALID_OPERATORS = ["+", "-", "*", "/", "//", "**"]
+
+ERROR_MESSAGE = ("\n    Your input was not validated. Make sure you\n"
+    "    use proper grouping rules, and that your input is:\n"
+    "    a number (integers like \"42\" or decimals like \"3.14159\");\n"
+    f"    an operator {VALID_OPERATORS};\n"
+    "    or \"delete\", \"quit\", or \"complete\"\n")
+
+equation = []
+types = []
+
+while True:
+    if equation:
+        print("".join(equation))
+
+    next_particle = input("===> ")
+
+    if next_particle == "quit":
+        break
+
+    if next_particle == "delete":
+        try:
+            del equation[-1]
+            del types[-1]
+        except IndexError:
+            print("You haven't entered anything yet!")
+        continue
+
+    if next_particle == "complete":
+        if equation:
+            print("".join(equation) + f" = {calculate()}")
+            equation.clear()
+            types.clear()
+            print("Beginning work on a new calculation...")
+        else:
+            print("You haven't entered anything yet!")
+        continue
+
+    if next_particle.isnumeric():
+        if not proper_grouping("number"):
+            print(ERROR_MESSAGE)
+        else:
+            equation.append(next_particle)
+            types.append("integer")
+        continue
+
+    if valid_float(next_particle):
+        if not proper_grouping("number"):
+            print(ERROR_MESSAGE)
+        else:
+            equation.append(next_particle)
+            types.append("float")
+        continue
+
+    if next_particle in VALID_OPERATORS:
+        if not proper_grouping("operator"):
+            print(ERROR_MESSAGE)
+        else:
+            equation.append(next_particle)
+            types.append("operator")
+        continue
+
+#   If all else fails....
+    print(ERROR_MESSAGE)
+
+print("\nThank you for running float_calculator.py\n")
